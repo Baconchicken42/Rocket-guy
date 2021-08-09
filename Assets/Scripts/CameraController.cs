67dccae1896a853e186c89a2330b4de8eb7ca4d0
Lucas Thielmann <darthsidious@earthlink.net>
2021-08-08 22:00:43 -0400

    added working tilemap, placed player in world, got camera to follow player, got spikes script working

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(player.transform.position.x + .85f, player.transform.position.y + 1.7f, this.transform.position.z);
+    }
+}
--- Assets/Scripts/SpikesController.cs
+++ Assets/Scripts/SpikesController.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class SpikesController : MonoBehaviour
 {
-    private BoxCollider2D collider;
+    public GameObject player;
+    public GameObject respawn;
+
+    private BoxCollider2D boxCol;
 
     // Start is called before the first frame update
     void Start()
     {
-        collider = 
+        boxCol = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision!");
+        if (collision.gameObject == player)
+        {
+            hurtPlayer();
+        }
+    }
+
+    void hurtPlayer()
+    {
+        player.transform.position = new Vector2(respawn.transform.position.x, respawn.transform.position.y);
+        Debug.Log("KILLED!!!");
     }
 }
