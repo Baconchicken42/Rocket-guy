5921f7d82a24a43240de933c7e2221c26972fec5
Lucas Thielmann <darthsidious@earthlink.net>
2021-08-08 23:18:27 -0400

    added victory treasure that loads a victory screen

---
+++ Assets/Scripts/DiamondController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DiamondController : MonoBehaviour
+{
+    public GameObject player;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision!");
+        if (collision.gameObject == player)
+        {
+            Debug.Log("Victory!");
+            SceneManager.LoadScene("End_Screen");
+        }
+    }
+}
